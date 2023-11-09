c1121e3371512637d213ed9a450c1d302b808791
Madalin Ciciu <madalinul@gmail.com>
2023-11-09 12:38:23 +0200

    delete email generation

--- .react-email/src/app/page.tsx
+++ .react-email/src/app/page.tsx
@@ -10,31 +10,31 @@ export default async function Index() {
 
   console.log('write emails');
 
-  for (const partner of partners) {
-    for (const email of emails) {
-      const Email = (await import(`../../../emails/${email}`)).default;
-      const markup = render(<Email partner={partner} />, { pretty: true });
-      const plainText = render(<Email partner={partner} />, {
-        plainText: true,
-      });
-      await fs.writeFile(
-        `../templates/${partner}/html/${email}.html`,
-        markup,
-        function (err) {
-          if (err) throw err;
-          console.log('Replaced!');
-        },
-      );
-      await fs.writeFile(
-        `../templates/${partner}/text/${email}.txt`,
-        plainText,
-        function (err) {
-          if (err) throw err;
-          console.log('Replaced!');
-        },
-      );
-    }
-  }
+  // for (const partner of partners) {
+  //   for (const email of emails) {
+  //     const Email = (await import(`../../../emails/${email}`)).default;
+  //     const markup = render(<Email partner={partner} />, { pretty: true });
+  //     const plainText = render(<Email partner={partner} />, {
+  //       plainText: true,
+  //     });
+  //     await fs.writeFile(
+  //       `../templates/${partner}/html/${email}.html`,
+  //       markup,
+  //       function (err) {
+  //         if (err) throw err;
+  //         console.log('Replaced!');
+  //       },
+  //     );
+  //     await fs.writeFile(
+  //       `../templates/${partner}/text/${email}.txt`,
+  //       plainText,
+  //       function (err) {
+  //         if (err) throw err;
+  //         console.log('Replaced!');
+  //       },
+  //     );
+  //   }
+  // }
 
   return <Home navItems={emails} />;
 }
